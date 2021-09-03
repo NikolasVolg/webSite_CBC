fa08f8c81e82825e9b6d3018ed88e43de4f060ce
NikoVM <nicolas.bornot@gmail.com>
2021-09-03 20:24:45 +0200

    réglage des ancres scroll-margin-top

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
 const $nav = document.getElementById('nav');
 const $logo = document.getElementById('logo');
 
-window.addEventListener('scroll', () =>
-    window.pageYOffset <= 90 ? $nav.classList.remove('scrolled') : $nav.classList.add('scrolled')
-);
-
-window.addEventListener('scroll', () =>
-    window.pageYOffset <= 90 ? $logo.classList.remove('logoScrolled') : $logo.classList.add('logoScrolled')
-);
+window.addEventListener('scroll', () => {
+    window.pageYOffset <= 80 ? $nav.classList.remove('scrolled') : $nav.classList.add('scrolled');
+    window.pageYOffset <= 80 ? $logo.classList.remove('logoScrolled') : $logo.classList.add('logoScrolled');
+});
